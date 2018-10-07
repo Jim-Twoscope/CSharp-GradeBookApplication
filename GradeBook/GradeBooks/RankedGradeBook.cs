bca26ef63f68ecae96ef466eca3ec8c6e7d0a2f9
Jim.Harris <Jim.Harris@GB4EGPW06213PHC>
2018-10-07 19:03:28 +0100

    Complete module 2 - error spotted

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -16,19 +16,20 @@ namespace GradeBook.GradeBooks
 			if (Students.Count < 5)
 				throw new InvalidOperationException("Ranked grading requires at least 5 students.");
 
-			var threshold = (int)Math.Ceiling(Students.Count * .02);
-			var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => averageGrade).ToList();
+				var threshold = (int)Math.Ceiling(Students.Count * .02);
+				var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
 
-			if (grades[threshold-1] <= averageGrade)
+				if (grades[threshold-1] <= averageGrade)
 					return 'A';
-			if (grades[(threshold * 2) -1] <= averageGrade)  
-				return 'B';
-			if (grades[(threshold * 3) -1] <= averageGrade)
-				return 'C';
-			if (grades[(threshold * 4) -1] <= averageGrade)
-				return 'D';
-			return ('F');
-
+				else if (grades[(threshold * 2) -1] <= averageGrade)  
+					return 'B';
+				else if (grades[(threshold * 3) -1] <= averageGrade)
+					return 'C';
+				else if (grades[(threshold * 4) -1] <= averageGrade)
+					return 'D';
+				else
+				return ('F');
+			
 		}
 	}
 }
